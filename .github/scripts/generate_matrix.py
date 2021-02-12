73ebaa8b6ff833429450fc49b4aa642a4a4ec5ee
Viktor Ashirov <vashirov@redhat.com>
2021-02-12 13:36:47 +0100

    Issue 4577 - Add GitHub actions
    Description:
    
    * Split build and test steps
    * Add ASAN variant for tests
    * Add HTML report for pytest, that contains ASAN/LSAN reports
    
    Relates: https://github.com/389ds/389-ds-base/issues/4577
    
    Reviewed by:

--- .github/scripts/generate_matrix.py
+++ .github/scripts/generate_matrix.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import json
+from itertools import product
 
 suites = next(os.walk('dirsrvtests/tests/suites/'))[1]
 
@@ -9,11 +10,20 @@ suites.remove('snmp')
 
 # Run each replication test module separately to speed things up
 suites.remove('replication')
-repl_tests = glob.glob('dirsrvtests/tests/suites/replication/*_test.py')
-suites += [repl_test.replace('dirsrvtests/tests/suites/', '') for repl_test in repl_tests]
 suites.sort()
+repl_tests = glob.glob('dirsrvtests/tests/suites/replication/*_test.py')
+# Put replication tests first as they are the longest
+suites = [repl_test.replace('dirsrvtests/tests/suites/', '') for repl_test in repl_tests] + suites
 
-suites_list = [{ "suite": suite} for suite in suites]
-matrix = {"include": suites_list}
+variants = ['gcc', 'gcc-asan']
+# Disable clang matrix for now, otherwise we generate more than 256 jobs...
+# variants += ['clang', 'clang-asan']
+
+
+jobs = []
+for suite, variant in product(suites, variants):
+    jobs.append({"suite": suite, "variant": variant})
+
+matrix = {"include": jobs}
 
 print(json.dumps(matrix))
